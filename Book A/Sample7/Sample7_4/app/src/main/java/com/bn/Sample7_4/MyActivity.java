5b32eddb66628114e585ebf0e771f0f7a6d60c0e
xiangfeng shen <xiangfeng.shen@outlook.com>
2022-10-16 16:57:38 +0800

    update Sample7_4 project

--- Book A/Sample7/Sample7_4/app/src/main/java/com/bn/Sample7_4/MyActivity.java
+++ Book A/Sample7/Sample7_4/app/src/main/java/com/bn/Sample7_4/MyActivity.java
@@ -13,90 +13,90 @@ import android.widget.RadioButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class MyActivity extends Activity {
-	private MySurfaceView mGLSurfaceView;
+    private MySurfaceView mGLSurfaceView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) 
+    protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);         
+        super.onCreate(savedInstanceState);
         //设置为全屏
-        requestWindowFeature(Window.FEATURE_NO_TITLE); 
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,  
-		              WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		//设置为横屏模式
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		//切换到主界面
-		setContentView(R.layout.main);	
-			
-		//初始化GLSurfaceView
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //设置为横屏模式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //切换到主界面
+        setContentView(R.layout.main);
+
+        //初始化GLSurfaceView
         mGLSurfaceView = new MySurfaceView(this);
         mGLSurfaceView.requestFocus();//获取焦点
-        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控  
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
         //将自定义的GLSurfaceView添加到外层LinearLayout中
-        LinearLayout ll=(LinearLayout)findViewById(R.id.main_liner); 
-        ll.addView(mGLSurfaceView);        
-        
+        LinearLayout ll=(LinearLayout)findViewById(R.id.main_liner);
+        ll.addView(mGLSurfaceView);
+
         //为RadioButton添加监听器
         RadioButton rb=(RadioButton)findViewById(R.id.Radi01);
         rb.setOnCheckedChangeListener(
-            new OnCheckedChangeListener()
-            {
-     			@Override
-     			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) 
-     			{
-     				if(isChecked)
-     				{
-     					mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[0];
-     					mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[4];
-     				}
-     			}        	   
-            }         		
+                new OnCheckedChangeListener()
+                {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
+                    {
+                        if(isChecked)
+                        {
+                            mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[0];
+                            mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[4];
+                        }
+                    }
+                }
         );
         rb=(RadioButton)findViewById(R.id.Radi02);
         rb.setOnCheckedChangeListener(
-            new OnCheckedChangeListener()
-            {
-     			@Override
-     			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) 
-     			{
-     				if(isChecked)
-     				{
-     					mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[1];
-     					mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[5];
-     				}
-     			}        	   
-            }         		
-        ); 
+                new OnCheckedChangeListener()
+                {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
+                    {
+                        if(isChecked)
+                        {
+                            mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[1];
+                            mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[5];
+                        }
+                    }
+                }
+        );
         rb=(RadioButton)findViewById(R.id.Radi03);
         rb.setOnCheckedChangeListener(
-            new OnCheckedChangeListener()
-            {
-     			@Override
-     			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) 
-     			{
-     				if(isChecked)
-     				{
-     					mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[2];
-     					mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[6];
-     				}
-     			}        	   
-            }         		
+                new OnCheckedChangeListener()
+                {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
+                    {
+                        if(isChecked)
+                        {
+                            mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[2];
+                            mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[6];
+                        }
+                    }
+                }
         );
         rb=(RadioButton)findViewById(R.id.Radi04);
         rb.setOnCheckedChangeListener(
-            new OnCheckedChangeListener()
-            {
-     			@Override
-     			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) 
-     			{
-     				if(isChecked)
-     				{
-     					mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[3];
-     					mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[7];
-     				}
-     			}        	   
-            }         		
+                new OnCheckedChangeListener()
+                {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
+                    {
+                        if(isChecked)
+                        {
+                            mGLSurfaceView.currenttexId32 = mGLSurfaceView.texId[3];
+                            mGLSurfaceView.currenttexId256 = mGLSurfaceView.texId[7];
+                        }
+                    }
+                }
         );
-        
+
     }
 
     @Override
@@ -109,7 +109,7 @@ public class MyActivity extends Activity {
     protected void onPause() {
         super.onPause();
         mGLSurfaceView.onPause();
-    }    
+    }
 }
 
 
